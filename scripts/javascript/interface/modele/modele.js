ea6ac3014a5b3320756b41edd31ae27ebbafa779
Guillaume Simmoneau <simonneaug@gmail.com>
2012-02-20 16:03:10 +0000

    typo
    git-svn-id: https://dev.isen.fr/svn/cir2/2011-2012/project/group12@84 d59c32fe-f752-4fa7-acd5-9dba6884753f

---
+++ scripts/javascript/interface/modele/modele.js
@@ -0,0 +1,44 @@
+var modele = {
+    
+    graph : new HackenbushGraph(),
+    
+    addNode : function(event) {
+        
+        var originalEvent = event.originalEvent;
+        console.log(originalEvent);
+        
+    },
+    
+    removeNode : function(event) {
+   
+        var originalEvent = event.originalEvent;
+        console.log(originalEvent);
+        
+    },
+    
+    addEdge : function(event, color) {
+        
+        var originalEvent = event.originalEvent;
+        console.log(originalEvent);
+        
+    },
+    
+    removeEdge : function(event) {
+        
+        var originalEvent = event.originalEvent;
+        console.log(originalEvent);
+        
+    },
+    
+    setEdgeColor : function(event, color) {
+        
+        var originalEvent = event.originalEvent;
+        console.log(originalEvent);
+        
+    },
+    
+    clear : function() {
+        
+    }
+    
+};
