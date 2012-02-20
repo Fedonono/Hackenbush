b4fada2848ceb03582e465209ec97f8ad6d414d8
Guillaume Simmoneau <simonneaug@gmail.com>
2012-02-20 14:26:48 +0000

    git-svn-id: https://dev.isen.fr/svn/cir2/2011-2012/project/group12@77 d59c32fe-f752-4fa7-acd5-9dba6884753f

---
+++ scripts/javascript/interface/controller/controller.js
@@ -0,0 +1,26 @@
+var controller = {
+	
+    listen : function(){
+        
+        $(".colorChooser").click( function(event) {
+            var element = event.currentTarget;
+            this.color = element.id;
+			
+        });
+		
+		
+        $(".toolChooser").click( function(event) {
+            var element = event.currentTarget;
+            this.tool = element.id;
+        
+        });
+    },
+	
+        
+    color : "blue", // default color : primary blue
+	
+    tool : null // default tool : none 
+    
+}
+
+controller.listen();
