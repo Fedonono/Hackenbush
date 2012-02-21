a0e27f8a98076478fad6627a41f42c0216072823
Guillaume Simmoneau <simonneaug@gmail.com>
2012-02-21 20:23:56 +0000

    gros boulversement dans me MVC
    git-svn-id: https://dev.isen.fr/svn/cir2/2011-2012/project/group12@96 d59c32fe-f752-4fa7-acd5-9dba6884753f

---
+++ scripts/javascript/interface/view/EdgeUI.js
@@ -0,0 +1,8 @@
+var EdgeUI = function( color, start, goal, index) {
+	
+	this.color = color;
+	this.start = start; // id
+	this.goal = goal;   // id
+        this.index = index  // his index in the edge table containig all edges between two nodes.
+	
+}
---
+++ scripts/javascript/interface/view/drawingArea.js
@@ -0,0 +1,48 @@
+(function(){
+
+    var canvas = $("#canvasArea");
+    var context = canvas[0].getContext('2d');
+
+    window.drawingArea = {
+    
+        canvas : canvas,
+    
+        context : context,
+    
+        imageData : context.getImageData(0, 0, canvas[0].width, canvas[0].height),
+    
+        items : {
+            nodes : new Array(),
+            edges : new Array()
+        },
+    
+        drawNode : function(x, y){
+
+            var context = drawingArea.context;
+            
+            context.beginPath();
+            context.lineWidth= "2";
+            context.fillStyle = "#FFD3F9";
+            context.arc(x, y, 6, 0, 2 * Math.PI);
+            context.stroke();
+            context.fill();
+            context.closePath();
+            
+        },
+    
+        addNode : function(nodeUi) {
+        
+            drawingArea.items.nodes["#"+nodeUi.id] = nodeUi;
+            drawingArea.items.nodes.length++;
+            
+            drawingArea.drawNode(nodeUi.x, nodeUi.y);
+        
+        },
+    
+        refresh : function() {
+        
+        }
+    
+    };
+    
+})()
