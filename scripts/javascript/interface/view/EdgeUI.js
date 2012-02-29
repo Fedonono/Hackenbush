0181f0487e7cf87467024b08c854fd47431fb81f
Guillaume Simmoneau <simonneaug@gmail.com>
2012-02-29 22:53:16 +0000

    git-svn-id: https://dev.isen.fr/svn/cir2/2011-2012/project/group12@103 d59c32fe-f752-4fa7-acd5-9dba6884753f

--- scripts/javascript/interface/view/EdgeUI.js
+++
@@ -1,8 +0,0 @@
-var EdgeUI = function( color, start, goal, index) {
-	
-	this.color = color;
-	this.start = start; // id
-	this.goal = goal;   // id
-        this.index = index  // his index in the edge table containig all edges between two nodes.
-	
-}
