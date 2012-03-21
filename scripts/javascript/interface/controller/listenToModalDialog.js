343d7505cb6ff2525489654cc11207202712abc4
Fedonono <arnaud.babol@gmail.com>
2012-03-21 21:37:40 +0000

    MVC !
    git-svn-id: https://dev.isen.fr/svn/cir2/2011-2012/project/group12@176 d59c32fe-f752-4fa7-acd5-9dba6884753f

--- scripts/javascript/interface/controller/listenToModalDialog.js
+++ scripts/javascript/interface/controller/listenToModalDialog.js
@@ -5,38 +5,8 @@
 	var name = $( "#name" ),
 		allFields = $( [] ).add( name ),
 		tips = $( ".validateTips" );
-
-	function updateTips( t ) {
-		tips
-			.text( t )
-			.addClass( "ui-state-highlight" );
-		setTimeout(function() {
-			tips.removeClass( "ui-state-highlight", 1500 );
-		}, 500 );
-	}
-
-	function checkLength( o, n, min, max ) {
-		if ( o.val().length > max || o.val().length < min ) {
-			o.addClass( "ui-state-error" );
-			updateTips( "Length of " + n + " must be between " +
-				min + " and " + max + "." );
-			return false;
-		} else {
-			return true;
-		}
-	}
-
-	function checkRegexp( o, regexp, n ) {
-		if ( !( regexp.test( o.val() ) ) ) {
-			o.addClass( "ui-state-error" );
-			updateTips( n );
-			return false;
-		} else {
-			return true;
-		}
-	}
 	
-	$( "#save-from" ).dialog({
+	$('#save-form').dialog({
 		autoOpen: false,
 		height: 228,
 		width: 350,
@@ -67,18 +37,7 @@
 		}
 	});
 
-	$( "#save" )
-		.click(function() {
-			$( "#save-from" ).dialog( "open" );
-		});
-
-	$( "#load" )
-		.click(function() {
-			$( "#load-from" ).dialog( "open" );
-			$("#load-from").load("./scripts/php/view/loadGame.php");
-		});
-
-	$( "#load-from" ).dialog({
+	$('#load-form').dialog({
 		autoOpen: false,
 		height: 500,
 		width: 800,
--- scripts/javascript/interface/controller/listenToTools.js
+++ scripts/javascript/interface/controller/listenToTools.js
@@ -1,75 +1,28 @@
-
-(function(){
+var controller = {
     
-    if(!window.controller) window.controller= new Object();
     
-    controller.playerColors = new Array();
-    
-    controller.color = "green"; // default color : primary green
+    color : "green", // default color : primary green
 	
-        controller.tool = "draw"; // default tool : draw
+    tool : "draw", // default tool : draw
     
     
-        controller.listenToTools = function(){
+    listenToTools : function(){
         
-            $(".colorChooser").click( function(event) {
-                controller.color = event.currentTarget.id;			
-            });
+        $(".colorChooser").click( function(event) {
+            controller.color = event.currentTarget.id;			
+        });
 		
 		
-            $(".toolChooser").click( function(event) {
-                var toolSelected = event.currentTarget.id;
-                if (toolSelected === "edit" | toolSelected === "draw" | toolSelected === "erase") drawingArea.setCursor(toolSelected);
-                if(toolSelected === "eraseAll") controller.eraseAll();
-                else controller.tool = toolSelected;
-            });
+        $(".toolChooser").click( function(event) {
+			var toolSelected = event.currentTarget.id;
+			if (toolSelected === "edit" | toolSelected === "draw" | toolSelected === "erase") drawingArea.setCursor(toolSelected);
+            if(toolSelected === "eraseAll") editionField.eraseAll();
+			if(toolSelected === "save") $('#save-form').dialog( "open" );
+			if(toolSelected === "load") { var load-form = $('#load-form'); load-form.dialog( "open" ); load-form.load("./scripts/php/view/loadGame.php"); }
+			else controller.tool = toolSelected;
+        });
         
-        };
-    
-    controller.initPlayerColors = function(){
-        var player1 = $("#player1");
-        var player2 = $("#player2");
-        var both = $("#bothPlayers");
-        controller.playerColors[0] = both[0].value;
-        controller.playerColors[1] = player1[0].value;
-        controller.playerColors[2] = player2[0].value;
-    }
-        
-    controller.setPlayerColors = function(playerInt) {
-        // VAR
-        var player1 = $("#player1");
-        var player2 = $("#player2");
-        var both = $("#bothPlayers");
-        var playerColors = controller.playerColors;
-        var player;
-            
-        if(playerInt === 0) player = both;
-        else if(playerInt === 1) player = player1;
-        else if(playerInt === 2) player = player2;
-        else return;
-        //FUNCTION
-        function swap(array, i, j){
-            var temp = array[i];
-            array[i] = array[j];
-            array[j] = temp;
-        }
-        function preventFromSameColor(){
-            for(var i = 0; i < playerColors.length; i++){
-                if(i !== playerInt){
-                    if(playerColors[i] === player[0].value) swap(playerColors, i, playerInt);
-                }
-            }
-            playerColors[playerInt] = player[0].value;
-            player1[0].value = playerColors[1];
-            player2[0].value = playerColors[2];
-            both[0].value = playerColors[0];
-        }
-        preventFromSameColor();
-    };
-    controller.initPlayerColors();
-    controller.listenToTools();
-    
-})()
-
-
+    }    
+};
+controller.listenToTools();
 
