d1217ea8eba1ac07ed7e41796c58630210546a72
Guillaume Simmoneau <simonneaug@gmail.com>
2012-03-21 20:31:42 +0000

    git-svn-id: https://dev.isen.fr/svn/cir2/2011-2012/project/group12@170 d59c32fe-f752-4fa7-acd5-9dba6884753f

---
+++ scripts/javascript/interface/view/BezierCurve.js
@@ -0,0 +1,8 @@
+var BezierCurve = function(orientationPoint1, orientationPoint2, color){
+    
+    this.beginOrientationPoint = orientationPoint1;
+    this.endOrientationPoint = orientationPoint2;
+    
+    this.color = color;
+    
+};
